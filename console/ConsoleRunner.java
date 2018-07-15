e9d76770ae59bc353c0224d867c4d409a374b2bd
Steven B <stevenberdak@gmail.com>
2018-07-15 05:06:51 -0700

    Added features

--- console/ConsoleRunner.java
+++ console/ConsoleRunner.java
@@ -1,8 +1,10 @@
-package console;
+package com.triquesoft.slydables.wordlibgen.console;
 
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.logging.Level;
@@ -11,34 +13,77 @@ import java.util.logging.Logger;
 /**
  * Sets up a console environment in a way which does not block the main thread.
  */
-public class ConsoleRunner {
+public class ConsoleRunner extends Observable {
 
+    public static final String KEEP_ALIVE_INTERRUPTED = "keep_alive_interrupted";
     private InputStream mInputStream;
     private Scanner mScanner;
-    private Thread mThread;
+    private Thread mInputThread, mKeepAliveThread;
     private String mCommand;
     private String[] mTokens, mFlags;
     private HashMap<String, Consumer<String[]>> mCommandMap;
     private Logger mLogger;
     private boolean mIsValid;
+    private boolean mAwaitInput;
     private boolean mKeepAlive;
+    private Observer mObserver;
 
     /**
      * Constructor.
      *
      * @param inputStream The input stream to use for reading commands.
      */
-    public ConsoleRunner(String consoleName, InputStream inputStream) {
+    public ConsoleRunner(String consoleName, InputStream inputStream, Observer observer) {
         mLogger = Logger.getLogger(consoleName);
         this.mInputStream = inputStream;
+        this.mObserver = observer;
     }
+
+    /**
+     * Get the logger attached to this instance.
+     * @return A logger for logging.
+     */
+    public Logger getLogger() {
+        return mLogger;
+    }
+
+    /**
+     * Keeps the application process alive.
+     */
+    public void keepAlive() {
+        mKeepAlive = true;
+        mKeepAliveThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("test");
+                while (mKeepAlive) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        mObserver.update(ConsoleRunner.this, KEEP_ALIVE_INTERRUPTED);
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        mKeepAliveThread.isDaemon();
+        mKeepAliveThread.start();
+    }
+
+    /**
+     * Allows the application to end process on it's own.
+     */
+    public void stopKeepAlive() {
+        mKeepAlive = false;
+    }
+
 
     /**
      * Resets the console back to its initial state.
      */
     public void reset() {
         stop();
-        mThread = null;
+        mInputThread = null;
         mCommand = null;
         mTokens = null;
         mFlags = null;
@@ -52,14 +97,14 @@ public class ConsoleRunner {
     public void start() {
         mScanner = new Scanner(mInputStream);
 
-        if (null == mThread) {
-            mThread = new Thread(new ConsoleTask());
-            mThread.setDaemon(true);
+        if (null == mInputThread) {
+            mInputThread = new Thread(new ConsoleTask());
+            mInputThread.setDaemon(true);
         }
 
-        mKeepAlive = true;
+        mAwaitInput = true;
 
-        if (!mThread.isAlive()) mThread.start();
+        if (!mInputThread.isAlive()) mInputThread.start();
         mLogger.log(Level.INFO, "Console initialized...");
     }
 
@@ -68,9 +113,9 @@ public class ConsoleRunner {
      */
     public void stop() {
 
-        mKeepAlive = false;
+        mAwaitInput = false;
 
-        mThread.interrupt();
+        mInputThread.interrupt();
         mLogger.log(Level.INFO, "Console stopped");
     }
 
@@ -96,13 +141,29 @@ public class ConsoleRunner {
     }
 
     /**
+     * Registers an observer to listen for events.
+     * @param observer Observer to be registered.
+     */
+    public void registerObserver(Observer observer) {
+        this.addObserver(observer);
+    }
+
+    /**
+     * Unregisters an observer from listening for events.
+     * @param observer Observer to be unregistered.
+     */
+    public void unregisterObserver(Observer observer) {
+        this.deleteObserver(observer);
+    }
+
+    /**
      * A simple task that awaits console input.
      */
     private class ConsoleTask implements Runnable {
 
         @Override
         public void run() {
-            while (mKeepAlive) {
+            while (mAwaitInput) {
                 mIsValid = true;
 
                 mCommand = mScanner.nextLine();
@@ -122,9 +183,10 @@ public class ConsoleRunner {
 
                 try {
                     if (mIsValid && mCommandMap != null && mCommandMap.containsKey(mTokens[0]))
-                        mCommandMap.get(mTokens[0]).accept(mFlags);
-                    else if (!mCommand.equals("")) mLogger.log(Level.WARNING, "Command not recognized. Please " +
-                            "check usage and try again. Proper syntax is <Command> -<flag> (ex: print -hello)");
+                       mCommandMap.get(mTokens[0]).accept(mFlags);
+                    else if (!mCommand.equals(""))
+                        mLogger.log(Level.WARNING, "Command not recognized. Please " +
+                                "check usage and try again. Proper syntax is <Command> -<flag> (ex: print -hello)");
                 } catch (NullPointerException e) {
                     mLogger.log(Level.WARNING, "There was a problem with the specified command");
                 }
